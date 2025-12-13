6dd017acda77fe41b82371fc72d84756e8468cdb
sabrina <xiongqiuyi318@gmail.com>
2025-12-13 16:59:04 +0100

    fix: 移除未使用的导入修复Vercel构建

--- src/components/EventCardCompact.tsx
+++ src/components/EventCardCompact.tsx
@@ -1,7 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 import { Event } from '../types';
-import { format } from 'date-fns';
-import { zhCN } from 'date-fns/locale';
 import './EventCardCompact.css';
 
 interface EventCardCompactProps {
